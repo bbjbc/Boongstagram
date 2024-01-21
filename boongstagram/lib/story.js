34a089263b915a33ea142417dd0dea23ca9c6169
bbjbc <jobc4550@gmail.com>
2024-01-22 00:44:33 +0900

    refactor: mongodb를 사용하여 데이터 가져오는 로직 변경

--- boongstagram/lib/story.js
+++ boongstagram/lib/story.js
@@ -1,12 +1,54 @@
-import sql from "better-sqlite3";
+// import sql from "better-sqlite3";
 
-const db = sql("story.db");
+// const db = sql("story.db");
+
+// export async function getStory() {
+//   await new Promise((resolve) => setTimeout(resolve, 2000));
+//   return db.prepare("SELECT * FROM story").all();
+// }
+
+// export function getStoryDetail(storySlug) {
+//   return db.prepare("SELECT * FROM story WHERE slug = ?").get(storySlug);
+// }
+
+import { MongoClient } from "mongodb";
 
 export async function getStory() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  return db.prepare("SELECT * FROM story").all();
+  const uri =
+    "mongodb+srv://aoo4550:PVKNU0YNANPV098D@cluster0.u8voidr.mongodb.net/story?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    // console.log("데이터베이스에 연결 시도");
+
+    const database = client.db();
+    const collection = database.collection("story");
+
+    const stories = await collection.find().toArray();
+    return stories;
+  } finally {
+    await client.close();
+    // console.log("닫힘");
+  }
 }
 
-export function getStoryDetail() {
-  return db.prepare("SELECT * FROM story WHERE slug = ?").get(slug);
+export async function getStoryDetail(storySlug) {
+  const uri =
+    "mongodb+srv://aoo4550:PVKNU0YNANPV098D@cluster0.u8voidr.mongodb.net/story?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    // console.log("데이터베이스 연결 시도");
+
+    const database = client.db();
+    const collection = database.collection("story");
+
+    const story = await collection.findOne({ slug: storySlug });
+    return story;
+  } finally {
+    await client.close();
+    // console.log("닫힘");
+  }
 }
