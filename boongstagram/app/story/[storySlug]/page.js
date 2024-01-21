ae2dc9bdb3c36db855930f1f5e05b85eb1a7d49f
bbjbc <jobc4550@gmail.com>
2024-01-22 00:43:55 +0900

    refactor: 사진이 배열일 경우 이미지 슬라이드 추가

--- boongstagram/app/story/[storySlug]/page.js
+++ boongstagram/app/story/[storySlug]/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import { notFound } from "next/navigation";
 
-import { getStory } from "@/lib/story";
+import { getStoryDetail } from "@/lib/story";
+import ImageSlide from "@/components/images/image-slide";
 
-export function generateMetadata({ params }) {
-  const story = getStory(params.storySlug);
+export async function generateMetadata({ params }) {
+  const story = await getStoryDetail(params.storySlug);
 
   if (!story) {
     notFound();
@@ -16,37 +16,39 @@ export function generateMetadata({ params }) {
   };
 }
 
-export default function StoryDetailsPage({ params }) {
-  const story = getStory(params.storySlug);
+export default async function StoryDetailsPage({ params }) {
+  const story = await getStoryDetail(params.storySlug);
 
   if (!story) {
     notFound();
   }
 
+  story.summary = story.summary.replace(/\n/g, "<br />");
+
   return (
     <>
-      <header className="flex items-center justify-center mb-8">
-        <div className="text-center">
-          <Image
-            src={story.image}
-            alt={story.title}
-            fill
-            className="w-64 h-64 object-cover rounded-full"
-          />
+      <header className="flex items-center justify-center mt-10 mb-8 mx-20 flex-col text-center">
+        <div>
+          <ImageSlide image={story.image} alt={story.title} />
         </div>
-        <div className="ml-8 text-left">
+        <div className="ml-8 mt-4 text-left">
           <h1 className="text-4xl font-bold mb-2">{story.title}</h1>
           <p className="text-sm text-[black]">{story.date}에 작성됨</p>
         </div>
       </header>
-      <main className="bg-gray-100 p-4 rounded">
-        <p
-          className="text-lg"
-          dangerouslySetInnerHTML={{
-            __html: story.summary,
-          }}
-        ></p>
-      </main>
+      <div className="pb-4">
+        <div className="relative bg-gradient-to-r from-green-700 to-lime-600 rounded-lg p-8 mx-auto my-12 w-2/4 text-center">
+          <p
+            className="text-lg leading-relaxed text-white"
+            dangerouslySetInnerHTML={{
+              __html: story.summary,
+            }}
+          ></p>
+          <div className="absolute top-0 left-0 p-4">
+            <p className="text-sm italic text-white">Content</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ boongstagram/components/images/image-slide.js
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropleft } from "react-icons/io";
+
+import classes from "./image-slide.module.css";
+import Image from "next/image";
+
+export default function ImageSlide({ image, alt }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === image.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? image.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className={classes["image-slider"]}>
+      <div
+        className={classes["image-container"]}
+        style={{
+          transform: `translateX(${-currentImageIndex * 100}%)`,
+        }}
+      >
+        {Array.isArray(image) ? (
+          image.map((img, index) => (
+            <div key={index} className={classes["image-item"]}>
+              <Image
+                src={img}
+                alt={alt}
+                width={500}
+                height={480}
+                className="rounded-xl"
+                priority
+              />
+            </div>
+          ))
+        ) : (
+          <Image
+            src={image}
+            alt={alt}
+            width={500}
+            height={480}
+            className="rounded-xl"
+            className={classes["image-item"]}
+          />
+        )}
+      </div>
+      {Array.isArray(image) && (
+        <>
+          <button onClick={prevImage} className={classes["prev-button"]}>
+            <IoIosArrowDropleft className="size-8" />
+          </button>
+          <button onClick={nextImage} className={classes["next-button"]}>
+            <IoIosArrowDropright className="size-8" />
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
