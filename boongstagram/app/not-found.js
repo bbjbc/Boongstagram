d5c09a00e8271bc3e45075613e8094a46eccdee5
bbjbc <jobc4550@gmail.com>
2024-01-23 22:15:10 +0900

    feat: not-found, error 페이지 추가 및 css 조정

---
+++ boongstagram/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>안타깝게도 요청 페이지를 찾을 수 없습니다.</p>
+    </main>
+  );
+}
---
+++ boongstagram/app/story/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>에러가 발생했습니다!</h1>
+      <p>데이터를 가져오는 데 실패했습니다. 잠시 뒤에 시도 해주세요!</p>
+    </main>
+  );
+}
