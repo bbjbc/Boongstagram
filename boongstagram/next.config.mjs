0d41aa0c3b74615c1832029eada8bf197b2b8249
bbjbc <jobc4550@gmail.com>
2024-01-25 02:00:11 +0900

    feat: aws s3 url 허용

--- boongstagram/next.config.mjs
+++ boongstagram/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "boongranii-nextjs-boongstagram.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
